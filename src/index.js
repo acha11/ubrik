6730e503758372246634a5f7736308ea3de023e3
andrew.chalmers <ajchalmers@gmail.com>
2022-01-01 15:12:01 +1100

    Full cube, random moves

--- src/index.js
+++ src/index.js
@@ -12,7 +12,9 @@ import {
     DirectionalLight,
     MeshBasicMaterial,
     FaceColors,
-    Group
+    Group,
+    BoxGeometry,
+    Matrix4
 } from 'three';
     
 import { OrbitControls } from './OrbitControls.js';
@@ -20,22 +22,16 @@ import { GUI } from 'three/examples/jsm/libs/dat.gui.module.js';
 import Stats from 'three/examples/jsm/libs/stats.module.js';
 
 var clock = new Clock();
-var wad;
 
 var params = {
-    map: "E1M2",
-    loadMap: loadMap
+    reset: reset,
+    startRandomMoves: startRandomMoves
 };
-
-function loadMap() {
-    alert(`load map ${params.map}`);
-    renderToThreeJs(wad, params.map);
-}
 
 var gui = new GUI();
     
-gui.add(params, 'map').name('Map');
-gui.add(params, 'loadMap').name('Load map');
+gui.add(params, 'reset').name('Reset');
+gui.add(params, 'startRandomMoves').name('Random moves');
 
 var renderer;
 var stats;
@@ -55,6 +51,7 @@ var rotationState = {
 
 const stickerSize = 9;
 const cubeletSize = 10;
+const cubeletSeparation = cubeletSize * 1.5;
 
 var mat = 
     new MeshBasicMaterial({
@@ -69,13 +66,29 @@ var colors = [
     new Color((128 << 24) | (128 << 16) | (24 <<  8)  | ( 24 <<  0)), // ARGB red
     new Color((128 << 24) | (192 << 16) | (192 <<  8) | (192 <<  0)), // ARGB white
     new Color((128 << 24) | (192 << 16) | (192 <<  8) | ( 24 <<  0)), // ARGB yellow
-    new Color((128 << 24) | ( 24 << 16) | (192 <<  8) | ( 24 <<  0)), // ARGB green
+    new Color((128 << 24) | ( 24 << 16) | (128 <<  8) | ( 24 <<  0)), // ARGB green
     new Color((128 << 24) | ( 24 << 16) | ( 24 <<  8) | (192 <<  0)), // ARGB blue
     new Color((128 << 24) | (192 << 16) | ( 96 <<  8) | ( 24 <<  0))  // ARGB orange
 ];
 
 var totalElapsed = 0;
 var nextTimeToLog = 1;
+
+function reset() {
+    for (var i = 0; i < cubelets.length; i++) {
+        cubelets[i].rotation.x = 0;
+        cubelets[i].rotation.y = 0;
+        cubelets[i].rotation.z = 0;
+    }
+
+    rotationState.rotationInProgress = false;
+}
+
+function startRandomMoves() {
+    reset();
+
+    rotateRandomFaces();
+}
 
 function getWorldPosOfCubelet(c) {
     // group for rotation contains
@@ -112,14 +125,12 @@ function setupThreeJs() {
             
             if (Math.floor(totalElapsed) > nextTimeToLog) {
                 nextTimeToLog = Math.floor(totalElapsed) + 1;
-
-                console.log(getWorldPosOfCubelet(cubelets[0]));
             }
 
             controls.update(delta);
 
             if (rotationState.rotationInProgress) {
-                const RotationScaleFactor = .255;
+                const RotationScaleFactor = 2;
 
                 var scaledDelta = delta * RotationScaleFactor;
 
@@ -138,16 +149,23 @@ function setupThreeJs() {
                 for (var i = 0; i < rotationState.cubeletsToRotate.length; i++) {
                     var c = rotationState.cubeletsToRotate[i];
 
+                    var rotationMatrix = new Matrix4();
                     switch (rotationState.rotationAxis) {
-                        case 0: c.rotation.x += rotationToApply * rotationState.rotationDirection; break;
-                        case 1: c.rotation.y += rotationToApply * rotationState.rotationDirection; break;
-                        case 2: c.rotation.z += rotationToApply * rotationState.rotationDirection; break;
+                        case 0: rotationMatrix.makeRotationX(rotationToApply * rotationState.rotationDirection); break;
+                        case 1: rotationMatrix.makeRotationY(rotationToApply * rotationState.rotationDirection); break;
+                        case 2: rotationMatrix.makeRotationZ(rotationToApply * rotationState.rotationDirection); break;
                     }
+
+                    c.applyMatrix4(rotationMatrix);
                 }
 
                 if (isRotationComplete) {
                     // Stop any further rotation
                     rotationState.rotationInProgress = false;
+
+                    if (rotationState.completionCallback) {
+                        rotationState.completionCallback();
+                    }
                 }
             }
         }
@@ -169,7 +187,7 @@ function renderToThreeJs() {
     camera.position.set(0, 0, 400);
 
     controls = new OrbitControls(camera, renderer.domElement);
-    //controls.autoRotate = true;
+    controls.autoRotate = false;
     controls.enableDamping = true;
     controls.update();
 
@@ -184,6 +202,14 @@ function renderToThreeJs() {
     directionalLight.position.z = 1;
 
     scene.add(directionalLight);
+
+    const ShowGreenCore = false;
+    if (ShowGreenCore) {
+        const geometry = new BoxGeometry( 10, 10, 10 );
+        const material = new MeshBasicMaterial( {color: 0x00ff00} );
+        const cube = new Mesh( geometry, material );
+        scene.add( cube );
+    }
 
     buildScene(scene);
 }
@@ -285,66 +311,173 @@ function buildAndAddCubelet(scene, frontColorIndex, rightColorIndex, topColorInd
     return grpThatSetsRotationDuringAnimation;
 }
 
-function startRotation(affectedCubelets, rotationAxis, rotationDirection) {
+function startRotation(affectedCubelets, rotationAxis, rotationDirection, completionCallback) {
     rotationState.cubeletsToRotate = affectedCubelets;
 
     rotationState.rotationAxis = rotationAxis;
     rotationState.rotationDirection = rotationDirection;
+    rotationState.currentRotationDelta = 0;
 
     rotationState.rotationInProgress = true;
+
+    rotationState.completionCallback = completionCallback;
 }
 
 function rotateCubelets(shouldCubeletBeRotatedBasedOnPosition, axis, direction) {
-    var cubeletsToRotate = [];
+    return new Promise(resolve => {
+        var cubeletsToRotate = [];
 
-    // Find the cubelets on the right face
-    for (var i = 0; i < cubelets.length; i++) {
-        var pos = getWorldPosOfCubelet(cubelets[i]);
-
-        if (shouldCubeletBeRotatedBasedOnPosition(pos)) {
-            cubeletsToRotate.push(cubelets[i]);
+        // Find the cubelets on the face that's being rotated
+        for (var i = 0; i < cubelets.length; i++) {
+            var pos = getWorldPosOfCubelet(cubelets[i]);
+    
+            if (shouldCubeletBeRotatedBasedOnPosition(pos)) {
+                cubeletsToRotate.push(cubelets[i]);
+            }
         }
-    }
-
-    startRotation(cubeletsToRotate, axis, direction );
+    
+        startRotation(cubeletsToRotate, axis, direction, resolve );
+    });
 }
 
+
+
 function rotateRightFaceCW() {
-    rotateCubelets(function (p) { return p.x > 19; }, 0, -1);
+    console.log("rotateRightFaceCW");
+    return rotateCubelets(function (p) { return p.x > cubeletSeparation; }, 0, -1);
 }
 
 function rotateRightFaceCCW() {
-    rotateCubelets(function (p) { return p.x > 19; }, 0, 1);
+    console.log("rotateRightFaceCCW");
+    return rotateCubelets(function (p) { return p.x > cubeletSeparation; }, 0, 1);
 }
 
 function rotateLeftFaceCW() {
-    rotateCubelets(function (p) { return p.x < -19; }, 0, 1);
+    console.log("rotateLeftFaceCW");
+    return rotateCubelets(function (p) { return p.x < -cubeletSeparation; }, 0, 1);
 }
 
 function rotateLeftFaceCCW() {
-    rotateCubelets(function (p) { return p.x < -19; }, 0, -1);
+    console.log("rotateLeftFaceCCW");
+    return rotateCubelets(function (p) { return p.x < -cubeletSeparation; }, 0, -1);
 }
 
 function rotateTopFaceCW() {
-    rotateCubelets(function (p) { return p.y > 19; }, 1, -1);
+    console.log("rotateTopFaceCW");
+    return rotateCubelets(function (p) { return p.y > cubeletSeparation; }, 1, -1);
 }
 
 function rotateTopFaceCCW() {
-    rotateCubelets(function (p) { return p.y > 19; }, 1, 1);
+    console.log("rotateTopFaceCCW");
+    return rotateCubelets(function (p) { return p.y > cubeletSeparation; }, 1, 1);
+}
+
+function rotateBottomFaceCW() {
+    console.log("rotateBottomFaceCW");
+    return rotateCubelets(function (p) { return p.y < -cubeletSeparation; }, 1, 1);
+}
+
+function rotateBottomFaceCCW() {
+    console.log("rotateBottomFaceCCW");
+    return rotateCubelets(function (p) { return p.y < -cubeletSeparation; }, 1, -1);
+}
+
+function rotateFrontFaceCW() {
+    console.log("rotateFrontFaceCW");
+    return rotateCubelets(function (p) { return p.z > cubeletSeparation; }, 2, -1);
+}
+
+function rotateFrontFaceCCW() {
+    console.log("rotateFrontFaceCCW");
+    return rotateCubelets(function (p) { return p.z > cubeletSeparation; }, 2, 1);
+}
+
+function rotateBackFaceCW() {
+    console.log("rotateBackFaceCW");
+    return rotateCubelets(function (p) { return p.z < -cubeletSeparation; }, 2, 1);
+}
+
+function rotateBackFaceCCW() {
+    console.log("rotateBackFaceCCW");
+    return rotateCubelets(function (p) { return p.z < -cubeletSeparation; }, 2, -1);
+}
+
+
+function rotateRandomFaces() {
+    var rotationNumber = Math.floor(Math.random() * 12);
+
+    var promise;
+
+    switch (rotationNumber)
+    {
+        case 0:  promise = rotateRightFaceCW();   break;
+        case 1:  promise = rotateRightFaceCCW();  break;
+        case 2:  promise = rotateLeftFaceCW();    break;
+        case 3:  promise = rotateLeftFaceCCW();   break;
+        case 4:  promise = rotateTopFaceCW();     break;
+        case 5:  promise = rotateTopFaceCCW();    break;
+        case 6:  promise = rotateBottomFaceCW();  break;
+        case 7:  promise = rotateBottomFaceCCW(); break;
+        case 8:  promise = rotateFrontFaceCW();   break;
+        case 9:  promise = rotateFrontFaceCCW();  break;
+        case 10: promise = rotateBackFaceCW();    break;
+        case 11: promise = rotateBackFaceCCW();   break;
+    }
+
+    promise.then(rotateRandomFaces);
 }
 
 function buildScene(scene) {
-    cubelets.push(buildAndAddCubelet(scene,    0, null,    2, null,    4, null, -1,  1,  1)); // top left
-    cubelets.push(buildAndAddCubelet(scene,    0, null,    2, null, null, null,  0,  1,  1)); // top middle
-    cubelets.push(buildAndAddCubelet(scene,    0,    1,    2, null, null, null,  1,  1,  1)); // top right
-    cubelets.push(buildAndAddCubelet(scene,    0, null, null, null,    4, null, -1,  0,  1)); // middle left
-    cubelets.push(buildAndAddCubelet(scene,    0, null, null, null, null, null,  0,  0,  1)); // centre
-    cubelets.push(buildAndAddCubelet(scene,    0,    1, null, null, null, null,  1,  0,  1)); // middle right
-    cubelets.push(buildAndAddCubelet(scene,    0, null, null, null,    4,    5, -1, -1,  1)); // bottom left
-    cubelets.push(buildAndAddCubelet(scene,    0, null, null, null, null,    5,  0, -1,  1)); // bottom middle
-    cubelets.push(buildAndAddCubelet(scene,    0,    1, null, null, null,    5,  1, -1,  1)); // bottom right
+    cubelets.push(buildAndAddCubelet(scene,    0, null,    2, null,    4, null, -1,  1,  1)); // front top left
+    cubelets.push(buildAndAddCubelet(scene,    0, null,    2, null, null, null,  0,  1,  1)); // front top middle
+    cubelets.push(buildAndAddCubelet(scene,    0,    1,    2, null, null, null,  1,  1,  1)); // front top right
+    cubelets.push(buildAndAddCubelet(scene,    0, null, null, null,    4, null, -1,  0,  1)); // front middle left
+    cubelets.push(buildAndAddCubelet(scene,    0, null, null, null, null, null,  0,  0,  1)); // front centre
+    cubelets.push(buildAndAddCubelet(scene,    0,    1, null, null, null, null,  1,  0,  1)); // front middle right
+    cubelets.push(buildAndAddCubelet(scene,    0, null, null, null,    4,    5, -1, -1,  1)); // front bottom left
+    cubelets.push(buildAndAddCubelet(scene,    0, null, null, null, null,    5,  0, -1,  1)); // front bottom middle
+    cubelets.push(buildAndAddCubelet(scene,    0,    1, null, null, null,    5,  1, -1,  1)); // front bottom right
 
-    rotateTopFaceCCW();
+    // frontColorIndex, rightColorIndex, topColorIndex, backColorIndex, leftColorIndex, bottomColorIndex, xOffset, yOffset, zOffset
+    cubelets.push(buildAndAddCubelet(scene, null, null,    2, null,    4, null, -1,  1,  0)); // middle top left
+    cubelets.push(buildAndAddCubelet(scene, null, null,    2, null, null, null,  0,  1,  0)); // middle top middle
+    cubelets.push(buildAndAddCubelet(scene, null,    1,    2, null, null, null,  1,  1,  0)); // middle top right
+    cubelets.push(buildAndAddCubelet(scene, null, null, null, null,    4, null, -1,  0,  0)); // middle left middle
+    // ain't no middle middle middle
+    cubelets.push(buildAndAddCubelet(scene, null,    1, null, null, null, null,  1,  0,  0)); // middle right middle    
+    cubelets.push(buildAndAddCubelet(scene, null, null, null, null,    4,    5, -1, -1,  0)); // middle bottom left
+    cubelets.push(buildAndAddCubelet(scene, null, null, null, null, null,    5,  0, -1,  0)); // middle bottom middle
+    cubelets.push(buildAndAddCubelet(scene, null,    1, null, null, null,    5,  1, -1,  0)); // middle bottom right
+
+
+    cubelets.push(buildAndAddCubelet(scene, null, null, null, null, null,    5,  0, -1,  0)); // middle bottom middle
+
+
+    cubelets.push(buildAndAddCubelet(scene, null, null,    2,    3,    4, null, -1,  1, -1)); // back top left
+    cubelets.push(buildAndAddCubelet(scene, null, null,    2,    3, null, null,  0,  1, -1)); // back top middle
+    cubelets.push(buildAndAddCubelet(scene, null,    1,    2,    3, null, null,  1,  1, -1)); // back top right
+    cubelets.push(buildAndAddCubelet(scene, null, null, null,    3,    4, null, -1,  0, -1)); // back middle left
+    cubelets.push(buildAndAddCubelet(scene, null, null, null,    3, null, null,  0,  0, -1)); // back centre
+    cubelets.push(buildAndAddCubelet(scene, null,    1, null,    3, null, null,  1,  0, -1)); // back middle right
+    cubelets.push(buildAndAddCubelet(scene, null, null, null,    3,    4,    5, -1, -1, -1)); // back bottom left
+    cubelets.push(buildAndAddCubelet(scene, null, null, null,    3, null,    5,  0, -1, -1)); // back bottom middle
+    cubelets.push(buildAndAddCubelet(scene, null,    1, null,    3, null,    5,  1, -1, -1)); // back bottom right
+
+//    rotateRandomFaces();
+
+    // Full work out, leaves in same state it started
+    // rotateTopFaceCCW()
+    //     .then(() => rotateTopFaceCW())
+    //     .then(() => rotateBottomFaceCW())
+    //     .then(() => rotateBottomFaceCCW())
+    //     .then(() => rotateLeftFaceCW())
+    //     .then(() => rotateLeftFaceCCW())
+    //     .then(() => rotateRightFaceCW())
+    //     .then(() => rotateRightFaceCCW())
+    //     .then(() => rotateBackFaceCW())
+    //     .then(() => rotateBackFaceCCW())
+    //     .then(() => rotateFrontFaceCW())
+    //     .then(() => rotateFrontFaceCCW());
 }
 
 setupThreeJs();
